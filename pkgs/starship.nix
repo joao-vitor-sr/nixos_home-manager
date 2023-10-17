0eccaecdf158507a810383d553b39fa4ac7e74c5
Joao Vitor Silveira Ribeiro <joaovitorsrs7@gmail.com>
2023-10-17 19:30:09 -0300

    new configuration

--- pkgs/starship.nix
+++ pkgs/starship.nix
@@ -4,11 +4,8 @@
     enable = true; 
     enableZshIntegration = true;
     settings = {
-      style = "default";
       add_newline = true;
       scan_timeout = 30;
-      keymap = "default";
-      disabled = false;
     };
   };
 }
--- pkgs/zsh.nix
+++ pkgs/zsh.nix
@@ -1,5 +1,4 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, ... }: {
   programs.zsh = {
     enable = true;
     shellAliases = {
@@ -23,15 +22,31 @@
       enable = true;
       plugins = [
         { name = "zsh-users/zsh-autosuggestions"; }
+        { name = "zsh-users/zsh-syntax-highlighting"; }
         { name = "zap-zsh/vim"; }
         { name = "hlissner/zsh-autopair"; }
       ];
     };
 
     initExtra = ''
-      bindkey '^y' autosuggest-accept
-      autoload -Uz compinit
-      compinit
+            bindkey '^y' autosuggest-accept
+            autoload -Uz compinit
+            compinit
+            
+      alias tmux='tmux -2'
+      function tmux_launch {
+          if [[ -z $TMUX ]]; then
+              TMUX_CON_SESSION=__dev__
+              if tmux has-session -t $TMUX_CON_SESSION 2>/dev/null; then
+                  tmux -f /etc/tmux.conf -2 attach-session -t $TMUX_CON_SESSION
+              else
+                  tmux -f /etc/tmux.conf -2 new-session -s $TMUX_CON_SESSION
+              fi
+          else
+          fi
+      }
+
+      alias tml='tmux_launch'
     '';
   };
   programs.fzf = {
