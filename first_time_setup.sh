52a93d5b182f2b94090d6e03e0fe8121cc72dfcd
Joao Vitor Silveira Ribeiro <joaovitorsrs7@gmail.com>
2023-10-17 22:21:12 -0300

    remove the wallpapers setup

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -16,33 +16,13 @@ install_nvchad ()
   ln -s $HOME/.config/home-manager/configs/nvchad/ $HOME/.config/nvim/lua/custom
 }
 
-install_wallpapers ()
-{
-  # get wallpapers from a directory on a public google drive
-  echo "Downloading wallpapers"
-  wget https://drive.google.com/file/d/1XVsLu3ayslExmpuYhh4TOQgsIqe_B9Ib/view?usp=share_link -O /tmp/wallpapers.zip
-
-  # validate if unzip is installed
-  if ! command -v unzip &> /dev/null
-  then
-      echo "unzip could not be found, please install it first"
-      exit
-  fi
-
-  echo "Unzipping wallpapers"
-  mkdir -p $HOME/pcs/
-  
-  unzip /tmp/wallpapers.zip -d $HOME/pcs/
-}
-
-echo "This script is aimed for the first time setup, it install some packages that are a little bit hard to install with home-manager\n"
-echo "are you sure you want to continue? (y/n)"
+echo "This script is aimed for the first time setup, it install some packages that are a little bit hard to install with home-manager\n\n"
+echo "are you sure you want to continue? (y/n)\n"
 
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then
   install_nvchad
-  install_wallpapers
   echo "Done!"
 else
     echo "Aborting..."
