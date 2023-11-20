32df4884e90f27529d3a32dbb4952cda04023c19
João Vitor Silveira Ribeiro <joaovitorsrs7@gmail.com>
2023-11-19 21:00:55 -0300

    refactor(zsh): new prompt

--- pkgs/zsh.nix
+++ pkgs/zsh.nix
@@ -1,9 +1,12 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.zsh = {
     enable = true;
+    autocd = true;
+    dotDir = ".config/zsh";
+    enableAutosuggestions = true;
+    enableCompletion = true;
     shellAliases = {
       j = "z";
-      f = "zi";
       g = "lazygit";
       ll = "lsd -l";
       ls = "lsd";
@@ -15,18 +18,38 @@
       clr = "clear";
       v = "$EDITOR";
     };
-    autocd = true;
-    enableAutosuggestions = true;
-    enableCompletion = true;
-    zplug = {
-      enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; }
-        { name = "zsh-users/zsh-syntax-highlighting"; }
-        { name = "zap-zsh/vim"; }
-        { name = "hlissner/zsh-autopair"; }
-      ];
-    };
+    plugins = with pkgs; [
+      {
+        name = "agkozak-zsh-prompt";
+        src = fetchFromGitHub {
+          owner = "agkozak";
+          repo = "agkozak-zsh-prompt";
+          rev = "v3.7.0";
+          sha256 = "1iz4l8777i52gfynzpf6yybrmics8g4i3f1xs3rqsr40bb89igrs";
+        };
+        file = "agkozak-zsh-prompt.plugin.zsh";
+      }
+      {
+        name = "zsh-syntax-highlighting";
+        src = fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "0.6.0";
+          sha256 = "0zmq66dzasmr5pwribyh4kbkk23jxbpdw4rjxx0i7dx8jjp2lzl4";
+        };
+        file = "zsh-syntax-highlighting.zsh";
+      }
+      {
+        name = "zsh-autopair";
+        src = fetchFromGitHub {
+          owner = "hlissner";
+          repo = "zsh-autopair";
+          rev = "34a8bca0c18fcf3ab1561caef9790abffc1d3d49";
+          sha256 = "1h0vm2dgrmb8i2pvsgis3lshc5b0ad846836m62y8h3rdb3zmpy1";
+        };
+        file = "autopair.zsh";
+      }
+    ];
 
     initExtra = ''
             bindkey '^y' autosuggest-accept
@@ -58,15 +81,6 @@
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
-  };
-
-  programs.starship = {
-    enable = true;
-    enableZshIntegration = true;
-    settings = {
-      add_newline = true;
-      scan_timeout = 30;
-    };
   };
 
 }
